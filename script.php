53b59e3afd98c6821d6437ed686655c6cfd94115
Mieszko Malawski <mieszko.malawski@getresponse.com>
2016-12-07 09:52:05 +0100

    solution2

--- script.php
+++ script.php
@@ -14,22 +14,9 @@
   return $result - $leftOver;
 }*/
 
-function danspower($num, $power) {
-     
-  $resultPower = pow($num, $power);
 
-if ($resultPower%2 == 1){
-  $resultat = (round($resultPower/10))*10;
 
-}else{
-  $resultat = $resultPower;
-}
-  return $resultat;
-}
-
-  
-
-/*function danspower($num, $power){
+function danspower($num, $power){
   $result = pow($num, $power);
   if($result % 2 === 0){
       return $result;
@@ -41,7 +28,7 @@ if ($resultPower%2 == 1){
      return $result + 10 - $leftOver;
   }
   return $result - $leftOver;
-}*/
+}
 
 $probe = \BlackfireProbe::getMainInstance();
 
