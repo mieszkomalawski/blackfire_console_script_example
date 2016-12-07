6eaf7305d08a14c993db52ec060bb54aa3743307
Mieszko Malawski <mieszko.malawski@getresponse.com>
2016-12-07 09:53:57 +0100

    solution3

--- script.php
+++ script.php
@@ -1,6 +1,6 @@
 <?php
 
-/*function danspower($num, $power) {
+function danspower($num, $power) {
   $result = pow($num, $power);
   if($result % 2 === 0){
       return $result;
@@ -10,22 +10,6 @@
   
   if($leftOver > 4){
      $result += 10;
-  }
-  return $result - $leftOver;
-}*/
-
-
-
-function danspower($num, $power){
-  $result = pow($num, $power);
-  if($result % 2 === 0){
-      return $result;
-  }
-  
-  $leftOver = $result % 10;
-  
-  if($leftOver > 4){
-     return $result + 10 - $leftOver;
   }
   return $result - $leftOver;
 }
